7f6c00fa4214d6316a0321649ed86c014e34dc01
Jae Park <jp1004.1040@gmail.com>
2024-09-26 21:05:03 -0400

    Change directory structure and create Spring Boot application for professor-rating

--- backend/README.md
+++
@@ -1,2 +0,0 @@
-# UGA Optimal Schedule Builder Project Backend Repo
-This repository will contain the backend development and structure for the UGA Optimized Schedule Builder project.
---
+++ services/professor-rating/src/main/java/edu/uga/devdogs/professor_rating/ProfessorRatingApplication.java
@@ -0,0 +1,13 @@
+package edu.uga.devdogs.professor_rating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorRatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorRatingApplication.class, args);
+	}
+
+}
