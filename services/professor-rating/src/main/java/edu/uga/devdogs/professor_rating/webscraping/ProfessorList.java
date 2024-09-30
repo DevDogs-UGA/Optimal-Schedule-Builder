f0e6ed29b95b2ed4b68bffbc70bcc334c945df11
Yuval Deshe <yuvaldeshe@gmail.com>
2024-09-30 16:14:41 -0400

    Changed variable names for clarity and removed null return values. System.err.print statements changed to use a Logger instead.

--- services/professor-rating/src/main/java/edu/uga/devdogs/professor_rating/webscraping/ProfessorList.java
+++ services/professor-rating/src/main/java/edu/uga/devdogs/professor_rating/webscraping/ProfessorList.java
@@ -1,6 +1,8 @@
 package edu.uga.devdogs.professor_rating.webscraping;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Arrays;
+import java.util.logging.Logger;
 
 /**
  * @author Yuval Deshe
@@ -8,7 +10,8 @@ import java.util.Arrays;
  * Represents the list of all professors at UGA on the RateMyProfessor website.
  */
 public class ProfessorList {
-    private ArrayList<String[]> professorList; // The list of professors. An entry in the professor
+    Logger logger = Logger.getLogger(getClass().getName()); // Used instead of printing to stderr.
+    private List<String[]> professorData; // The list of professors. An entry in the professor
                                                // list should always formatted as 
                                                // {professorName, professorID}.
 
@@ -16,7 +19,7 @@ public class ProfessorList {
      * Constructs a {@code ProfessorList} object.
      */
     public ProfessorList() {
-        professorList = new ArrayList<String[]>();
+        professorData = new ArrayList<>();
     } // ProfessorList
 
     /**
@@ -26,16 +29,16 @@ public class ProfessorList {
      * @param professorEntry an entry for one professor, formatted {professorName, professorID}.
      */
     public ProfessorList(String[]... professorEntry) {
-        professorList = new ArrayList<String[]>();
+        professorData = new ArrayList<>();
         // If no entries are specified, do not add anything to the list.
         if (professorEntry.length == 0) {
             return;
         } // if
         for (String[] professor : professorEntry) {
             if (professor.length == 2) {
-                professorList.add(professor);
+                professorData.add(professor);
             } else {
-                System.out.println("Invalid entry detected.");
+                logger.info("Invalid entry detected.");
                 return;
             } // if
         } // if
@@ -44,42 +47,44 @@ public class ProfessorList {
     /**
      * Constructs a {@code ProfessorList} object with a pre-made list specified as a parameter.
      * 
-     * @param professorList A list of professor entries, formatted {professorName, professorID}.
+     * @param professorData A list of professor entries, formatted {professorName, professorID}.
      */
-    public ProfessorList(ArrayList<String[]> professorList) {
-        this.professorList = professorList;
+    public ProfessorList(List<String[]> professorData) {
+        this.professorData = professorData;
     } // ProfessorList
     
     /**
      * Returns a professor entry with a matching name to the {@code professorName} parameter.
-     * Returns null if there is no professor matching the specified {@code professorName}.
+     * Returns an empty array if there is no professor matching the specified {@code professorName}.
      * 
      * @param professorName The name of the professor.
      * @return The entry for the professor.
      */
     public String[] getProfessorWithName(String professorName) {
-        for (String[] professor : professorList) {
+        for (String[] professor : professorData) {
             if (professor[0].equals(professorName)) {
                 return professor;
             } // if
         } // for
-        return null;
+        // Return an empty array if the professor was not found
+        return new String[2];
     } // getProfessorEntryWithName
 
     /**
      * Returns a professor entry with a matching name to the {@code professorID} parameter.
-     * Returns null if there is no entry matching the specified {@code professorID}.
+     * Returns an empty array if there is no entry matching the specified {@code professorID}.
      * 
      * @param professorID The name of the professor.
      * @return The entry for the professor.
      */
     public String[] getProfessorWithID(String professorID) {
-        for (String[] entry : professorList) {
+        for (String[] entry : professorData) {
             if (entry[1].equals(professorID)) {
                 return entry;
             } // if
         } // for
-        return null;
+        // Return an empty array if the professor was not found
+        return new String[2];
     } // getProfessorEntryWithID
 
     /**
@@ -89,7 +94,7 @@ public class ProfessorList {
      * @return True if the professor is in the list, or false if the professor is not in the list.
      */
     public boolean findProfessor(String[] professor) {
-        for (String[] entry : professorList) {
+        for (String[] entry : professorData) {
             if (Arrays.equals(entry, professor)) {
                 return true;
             } // if
@@ -98,16 +103,16 @@ public class ProfessorList {
     } // findProfessor
 
     /**
-     * Adds the specified professor to the {@code professorList}.
+     * Adds the specified professor to the {@code professorData}.
      * 
      * @param professor an entry for one professor, formatted {professorName, professorID}.
      */
     public void addProfessor(String[]... professors) {
         for (String[] professor : professors) {
             if (professor.length == 2) {
-                professorList.add(professor);
+                professorData.add(professor);
             } else {
-                System.out.println("Invalid entry detected.");
+                logger.info("Invalid entry detected.");
             } // if
         }
     } // addProfessor
