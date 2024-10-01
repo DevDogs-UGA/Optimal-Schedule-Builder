27d136bf3adb5636863cc684fe9af4826eed847d
Majd <majd.jrrar1@gmail.com>
2024-09-30 20:24:20 -0400

    initialized microservice layer

---
+++ services/professor-rating/src/main/java/edu/uga/devdogs/professor_rating/Services/ProfessorRatingService.java
@@ -0,0 +1,10 @@
+package edu.uga.devdogs.professor_rating.controllers;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProfessorRating {
+    public static void someBusinessLogic() {
+        // Service logic goes here
+    }
+}
