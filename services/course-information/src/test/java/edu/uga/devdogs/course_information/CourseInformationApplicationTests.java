8a134208d03d2be4c549cb3449f3e9bbbda20c60
andreigaz <agazinschi@gmail.com>
2024-10-01 13:12:58 -0400

    added course-information spring application

---
+++ services/course-information/src/test/java/edu/uga/devdogs/course_information/CourseInformationApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uga.devdogs.course_information;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseInformationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
