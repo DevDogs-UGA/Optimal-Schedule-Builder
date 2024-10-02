ed9824750b842fb92ac23fc42aadda8d607e2e25
LightningFast7 <anb33569@uga.edu>
2024-10-02 10:55:03 -0400

    Edited both files again

--- api/CourseController.java
+++ api/CourseController.java
@@ -1,13 +1,3 @@
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 public class CourseController {
     /**
