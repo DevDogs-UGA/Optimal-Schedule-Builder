615ec477ad9c28b3f35fe6ffd7b3b7d27feb5e1a
Ignas Panavas <76080358+IgnasPanavas@users.noreply.github.com>
2024-10-03 17:47:40 -0400

    Cleaned up controller implementation

--- services/professor-rating/src/main/java/edu/uga/devdogs/professor_rating/controllers/RESTController.java
+++ services/professor-rating/src/main/java/edu/uga/devdogs/professor_rating/controllers/RESTController.java
@@ -2,6 +2,7 @@ package edu.uga.devdogs.professor_rating.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
