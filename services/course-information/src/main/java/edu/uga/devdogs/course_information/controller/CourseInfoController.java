028d0d04bff9be1be1666a20bcfc56d78e002862
LightningFast7 <anb33569@uga.edu>
2024-10-10 18:48:19 -0400

    Implemented getCourseByProfessor

--- services/course-information/src/main/java/edu/uga/devdogs/course_information/controller/CourseInfoController.java
+++ services/course-information/src/main/java/edu/uga/devdogs/course_information/controller/CourseInfoController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Controller that handles the Course-Information details.
  * Provides courses based on parameters like major, professor,
@@ -14,6 +17,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/course_information")
 public class CourseInfoController {
    
+    /**
+     * Asks for list of course information that relates to the 
+     * @param professor given.
+     * 
+     * @param professor name of the professor teaching the course
+     * @return course information list that's given by the professor.
+     */
+    @GetMapping("/professor")
+   public List<Section> getCourseByProfessor(@RequestParam String professor){
+        List <Section> courseInfo = new ArrayList<>();
 
+        //courseInfo = professorCourse();
+        //professorCourse is a dummy function that provides a list of course information pertaining to the professor.
+
+        return courseInfo;
+   } 
     
 }
