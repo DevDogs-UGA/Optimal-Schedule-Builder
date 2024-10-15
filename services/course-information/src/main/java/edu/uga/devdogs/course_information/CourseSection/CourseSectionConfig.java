d57fbf1a387b598c11da231df911564e95c76ad1
Joey M <jmm35198@uga.edu>
2024-10-15 16:00:59 -0400

    made correct number of inputs in CourseSectionConfig

--- services/course-information/src/main/java/edu/uga/devdogs/course_information/CourseSection/CourseSectionConfig.java
+++ services/course-information/src/main/java/edu/uga/devdogs/course_information/CourseSection/CourseSectionConfig.java
@@ -10,10 +10,17 @@ public class CourseSectionConfig {
     CommandLineRunner commandLineRunner(CourseSectionRepository courseSectionRepository) {
         return args -> {
             CourseSection section1 = new CourseSection(
-                    "physiology",
-                    "420",
-                    "pain",
-                    "Mary Francis early education");
+                    123456,
+                    4,
+                    "something",
+                    1,
+                    4,
+                    "Barnes",
+                    3,
+                    40,
+                    12,
+                    3,
+                    1293);
 
             courseSectionRepository.save(section1);
         };
