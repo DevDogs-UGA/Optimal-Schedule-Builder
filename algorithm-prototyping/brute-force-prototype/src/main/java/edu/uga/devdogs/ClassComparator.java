b365dfa73992317160b4f0cd5399b940f9195d85
Aahil Lakhani <lakhaniaahil@gmail.com>
2024-10-16 00:48:37 -0400

    Implement ClassComparator

---
+++ algorithm-prototyping/brute-force-prototype/src/main/java/edu/uga/devdogs/ClassComparator.java
@@ -0,0 +1,13 @@
+package edu.uga.devdogs;
+
+import edu.uga.devdogs.records.Course.Section.Class;
+
+import java.util.Comparator;
+
+public class ClassComparator implements Comparator<Class> {
+
+    @Override
+    public int compare(Class class1, Class class2) {
+        return class1.startTime().compareTo(class2.startTime());
+    }
+}
