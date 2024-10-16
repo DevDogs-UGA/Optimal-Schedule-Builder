95d88756cbeeae08fab9e79ab900cdb47a003073
Aahil Lakhani <lakhaniaahil@gmail.com>
2024-10-16 03:14:09 -0400

    Rename ObjectiveCalculator to ComputeObjective

---
+++ algorithm-prototyping/brute-force-prototype/src/main/java/edu/uga/devdogs/ComputeObjective.java
@@ -0,0 +1,21 @@
+package edu.uga.devdogs;
+
+public class ComputeObjective {
+
+    private static float averageProfessorQuality(Schedule schedule) {
+
+    }
+
+    private static float maxDistance(Schedule schedule) {
+
+    }
+
+    private static float averageIdleTime(Schedule schedule) {
+
+    }
+
+    public static float overall(Schedule schedule, Float[] weights) {
+
+    }
+
+}
