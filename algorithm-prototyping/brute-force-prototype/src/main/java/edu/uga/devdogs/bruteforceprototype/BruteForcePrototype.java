75e341a415dbb1d8c8923e2276274834e70c5177
Aahil Lakhani <lakhaniaahil@gmail.com>
2024-10-17 15:59:46 -0400

    Create outline for BruteForcePrototype

--- algorithm-prototyping/brute-force-prototype/src/main/java/edu/uga/devdogs/bruteforceprototype/BruteForcePrototype.java
+++ algorithm-prototyping/brute-force-prototype/src/main/java/edu/uga/devdogs/bruteforceprototype/BruteForcePrototype.java
@@ -1,7 +1,25 @@
 package edu.uga.devdogs.bruteforceprototype;
 
+import edu.uga.devdogs.sampledataparser.records.Course;
+import edu.uga.devdogs.sampledataparser.records.Distances;
+import edu.uga.devdogs.sampledataparser.records.SampleData;
+import edu.uga.devdogs.sampledataparser.records.Section;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class BruteForcePrototype {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+    private final Course[] courses;
+    private final Distances distances;
+
+    public BruteForcePrototype(SampleData sampleData) {
+        this.courses = sampleData.courses();
+        this.distances = sampleData.distances();
     }
+
+    public Set<Section> optimize(Set<String> course_codes) {
+        return new HashSet<>();
+    }
+
 }
