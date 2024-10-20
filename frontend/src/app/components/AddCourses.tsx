d06a68b3ed33fcf3d178121ca6e716b73604d8ca
Jeffrey Luo <77336910+lunarsceals@users.noreply.github.com>
2024-10-20 01:19:34 -0400

    Add tab switching functionality to AddCourses component

--- frontend/src/app/components/AddCourses.tsx
+++ frontend/src/app/components/AddCourses.tsx
@@ -20,41 +20,46 @@ const TabButton = ({ label, onClick, className }: TabProps) => {
   );
 };
 
-const Tabs = () => {
+export const AddCourses = () => {
+  const [currentTab, setCurrentTab] = useState(0);
+  const tabs = ["section", "instructor", "crn"];
   const handleTabChange = (index: number) => {
     setCurrentTab(index);
   };
 
-  const [currentTab, setCurrentTab] = useState(0);
-  const tabs = ["section", "instructor", "crn"];
-  return (
-    <>
-      {tabs.map((tab, index) => (
-        <TabButton
-          key={index}
-          label={`By ${tab}`}
-          onClick={() => handleTabChange(index)}
-          className={
-            currentTab === index
-              ? "bg-bulldog-red text-white"
-              : "bg-dusty-pink text-[#CFBEBE]"
-          }
-        />
-      ))}
-    </>
-  );
-};
-
-export const AddCourses = () => {
   return (
     <div className="min-w-fit">
       <div className="flex gap-2">
-        <Tabs />
+        {tabs.map((tab, index) => (
+          <TabButton
+            key={index}
+            label={`By ${tab}`}
+            onClick={() => handleTabChange(index)}
+            className={
+              currentTab === index
+                ? "bg-bulldog-red text-white"
+                : "bg-dusty-pink text-[#CFBEBE]"
+            }
+          />
+        ))}
       </div>
       <main className="bg-barely-pink border-dusty-pink justify-between gap-4 border-2 px-12 py-8">
         <div className="items-center justify-center space-y-4 py-12">
-          <SearchSelector text="Subject" />
-          <SearchSelector text="Course" />
+          {currentTab === 0 ? (
+            <>
+              <SearchSelector text="Subject" />
+              <SearchSelector text="Course" />
+            </>
+          ) : currentTab === 1 ? (
+            <>
+              <SearchSelector text="Instructor" />
+              <SearchSelector text="Course" />
+            </>
+          ) : currentTab === 2 ? (
+            <>
+              <SearchSelector text="CRN" />
+            </>
+          ) : null}
         </div>
         <section className="flex justify-end gap-4">
           <Button text="Search" />
