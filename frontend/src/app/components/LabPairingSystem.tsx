3d28bd1cba9be104a2e135aa242dd0e89d2f1beb
Jay Patel <81924044+jayp822@users.noreply.github.com>
2024-10-21 18:43:09 -0400

    good

---
+++ frontend/src/app/components/LabPairingSystem.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function LabPairingSystem() {
+  return (
+    <div className="flex transition ease-in-out duration-300 bg-[#F3EDED] w-24 h-24 sm:w-32 sm:h-32 md:w-40 md:h-40 rounded-xl hover:bg-[#c3a5a5] items-center justify-center">
+      <p className="text-[10px] sm:text-[14px] md:text-[16px]">
+        Lab Pairing System
+      </p>
+    </div>
+  );
+}
---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "UGA Optimal Schedule Builder",
+  description:
+    "Optimal Schedule Builder web application for UGA students to generate and design optimal academic schedules",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
