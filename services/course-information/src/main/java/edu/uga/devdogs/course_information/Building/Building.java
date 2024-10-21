30e4d020c7a0f1cdda582439ad61e853cb936722
rin.bell <rin.bell4772@gmail.com>
2024-10-21 18:13:54 -0400

    Created Building Entity
    Created Building entity class
    
    To-do: Relationships

---
+++ services/course-information/src/main/java/edu/uga/devdogs/course_information/Building/Building.java
@@ -0,0 +1,93 @@
+package edu.uga.devdogs.course_information.Building;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.io.Serializable;
+
+/*
+ * Java JPA entity represention for Building
+ */
+@Entity
+public class Building implements Serializable {
+    
+    /*
+     * Variables
+     */
+
+     @Id
+    private int buildingNumber;
+
+    private String name;
+
+    private String grid;
+
+    /*
+     * Relationships
+     * To-Do: add relationships (one-to-one, one-to-many, many-to-one, many-to-many) here
+     */
+
+    /*
+     * Constructors
+     */
+
+    // Default constructor
+    public Building() {
+    }
+
+    // Constructor w/o buildingNumber
+    public Building(String name, String grid) {
+        this.name = name;
+        this.grid = grid;
+    }
+
+    // Constructor w/ buildingNumber
+    public Building(int buildingNumber, String name, String grid) {
+        this.buildingNumber = buildingNumber;
+        this.name = name;
+        this.grid = grid;
+    }
+
+    
+    /*
+     * Getters and Setters
+     */
+
+    public int getBuildingNumber() {
+        return buildingNumber;
+    }
+
+    public void setBuildingNumber(int buildingNumber) {
+        this.buildingNumber = buildingNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGrid() {
+        return grid;
+    }
+
+    public void setGrid(String grid) {
+        this.grid = grid;
+    }
+
+
+    /*
+     * toString
+     */
+    @Override
+    public String toString() {
+        return "Building [ buildingNumber=" + buildingNumber 
+                + ", name='" + name 
+                + ", grid='" + grid 
+                + ']';
+    }
+
+    
+}
