5a8f410b0c0e90423b17bb18b1fda6472ba98574
SFNDude123502 <102396379+SFNDude123502@users.noreply.github.com>
2024-10-28 14:26:45 -0400

    Update generate_distance_matrix.py

--- algorithm-prototyping/src/main/resources/scripts/generate_distance_matrix.py
+++ algorithm-prototyping/src/main/resources/scripts/generate_distance_matrix.py
@@ -5,20 +5,18 @@ with open('buildings.json', 'r') as file:
     data = json.load(file)
 
 # Initialize output dictionary with buildings array and distance matrix
-distanceData = {
-    'distance_matrix': {}
-}
+distanceData = {}
 
 #Calculate distance matrix and update output dictionary
-for building1 in data['buildings']:
-    distanceData['distance_matrix'][building1['name']] = {}
+for building1 in data:
+    distanceData[building1['name']] = {}
 
-    for building2 in data['buildings']:
+    for building2 in data:
         # Calculate distance between two buildings using formula: distance = sqrt(a^2 + b^2)
         # Where 'a' is the difference in x-coordinates and 'b' is the difference in y-coordinates
         dist = ((building1['x']-building2['x']) ** 2 + (building1['y']-building2['y']) ** 2) ** 0.5
         
-        distanceData['distance_matrix'][building1['name']][building2['name']] = dist
+        distanceData[building1['name']][building2['name']] = dist
 
 # Serializing json
 output = json.dumps(distanceData, indent=4)
