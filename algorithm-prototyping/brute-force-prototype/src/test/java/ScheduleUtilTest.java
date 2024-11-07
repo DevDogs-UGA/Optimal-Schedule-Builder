d42268b1d5d25d457b8141631d18888e46d5e480
yousef-0614 <133291362+yousef-0614@users.noreply.github.com>
2024-11-07 14:56:47 -0500

    Created 5 invalid test schedules in the testValidate() method.

--- algorithm-prototyping/brute-force-prototype/src/test/java/ScheduleUtilTest.java
+++ algorithm-prototyping/brute-force-prototype/src/test/java/ScheduleUtilTest.java
@@ -38,7 +38,42 @@ public class ScheduleUtilTest {
 
     @Test
     void testValidate() {
+        /*
+        Five invalid schedules that have time conflicts,
+        validate should return false with one of these passed in.
+         */
+        Schedule invalidOne = new Schedule(Set.of(
+                courses.get("ENGL 1101").sections().get(0),
+                courses.get("CSCI 1301").sections().get(0),
+                courses.get("PHYS 1211").sections().get(2)
+        ));
 
+        Schedule invalidTwo = new Schedule(Set.of(
+                courses.get("CHEM 1211").sections().get(0),
+                courses.get("CHEM 1211L").sections().get(0),
+                courses.get("MATH 2250").sections().get(0),
+                courses.get("CSCI 1302").sections.get(0)
+        ));
+
+        Schedule invalidThree = new Schedule(Set.of(
+                courses.get("MATH 2260").sections().get(0),
+                courses.get("ENGL 1101").sections().get(1),
+                courses.get("CSCI 1301").sections().get(0),
+                courses.get("PHYS 1211").sections.get(0)
+        ));
+
+        Schedule invalidFour = new Schedule(Set.of(
+                courses.get("PHYS 1212").sections().get(0),
+                courses.get("PHYS 1212L").sections().get(2),
+                courses.get("MATH 2700").sections().get(1),
+                courses.get("ENGL 1101").sections.get(0)
+        ));
+
+        Schedule invalidFive = new Schedule(Set.of(
+                courses.get("ENGL 1102").sections().get(1),
+                courses.get("CSCI 1302").sections().get(2),
+                courses.get("MATH 2250").sections().get(1),
+        ));
     }
 
     @Test
