4b3483540b8270f9a4e357f13d567d7279e3c1b0
KadeStyron <kjs00988@uga.edu>
2024-11-08 15:44:27 -0500

    updated course.java to include bulletin

--- services/course-information/src/main/java/edu/uga/devdogs/course_information/Course/Course.java
+++ services/course-information/src/main/java/edu/uga/devdogs/course_information/Course/Course.java
@@ -3,10 +3,13 @@ package edu.uga.devdogs.course_information.Course;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 
 import java.io.Serializable;
 import java.util.List;
+
+import org.hibernate.annotations.ManyToAny;
 
 import edu.uga.devdogs.course_information.CourseSection.CourseSection;
 
@@ -29,6 +32,161 @@ public class Course implements Serializable {
     //Relationships
     @OneToMany(mappedBy = "course")
     private List<CourseSection> courseSections;
+    
+    //variables for bulletin
+
+
+    @ManyToOne()
+    private int departmentid;
+
+    private String description;
+
+    private int credits;
+
+    private String prerequisites;
+
+    private String level;
+
+    private String schedule;
+
+    private String location;
+
+    private String semester;
+
+    private int year;
+
+    private int maxEnrollment;
+
+    private int currentEnrollment;
+
+    private int waitlistAvailable;
+
+    private int waitlistCount;
+
+    //marked as int in DB Design
+    private int isOnline;
+
+    private String gradeScale;
+
+    public int getDepartmentid() {
+        return departmentid;
+    }
+
+    public void setDepartmentid(int departmentid) {
+        this.departmentid = departmentid;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public void setCredits(int credits) {
+        this.credits = credits;
+    }
+
+    public String getPrerequisites() {
+        return prerequisites;
+    }
+
+    public void setPrerequisites(String prerequisites) {
+        this.prerequisites = prerequisites;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(String schedule) {
+        this.schedule = schedule;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getSemester() {
+        return semester;
+    }
+
+    public void setSemester(String semester) {
+        this.semester = semester;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMaxEnrollment() {
+        return maxEnrollment;
+    }
+
+    public void setMaxEnrollment(int maxEnrollment) {
+        this.maxEnrollment = maxEnrollment;
+    }
+
+    public int getCurrentEnrollment() {
+        return currentEnrollment;
+    }
+
+    public void setCurrentEnrollment(int currentEnrollment) {
+        this.currentEnrollment = currentEnrollment;
+    }
+
+    public int getWaitlistAvailable() {
+        return waitlistAvailable;
+    }
+
+    public void setWaitlistAvailable(int waitlistAvailable) {
+        this.waitlistAvailable = waitlistAvailable;
+    }
+
+    public int getWaitlistCount() {
+        return waitlistCount;
+    }
+
+    public void setWaitlistCount(int waitlistCount) {
+        this.waitlistCount = waitlistCount;
+    }
+
+    public int getIsOnline() {
+        return isOnline;
+    }
+
+    public void setIsOnline(int isOnline) {
+        this.isOnline = isOnline;
+    }
+
+    public String getGradeScale() {
+        return gradeScale;
+    }
+
+    public void setGradeScale(String gradeScale) {
+        this.gradeScale = gradeScale;
+    }
 
     //Constructors, getters, setters...
     public Course() {
