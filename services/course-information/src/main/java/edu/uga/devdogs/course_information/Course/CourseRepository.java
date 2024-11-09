b5b4dca35d963fba1f9bc2b68ed09f3e13c8053e
BryantMonahan <129926467+BryantMonahan@users.noreply.github.com>
2024-11-09 15:10:50 -0500

    Update CourseRepository.java

--- services/course-information/src/main/java/edu/uga/devdogs/course_information/Course/CourseRepository.java
+++ services/course-information/src/main/java/edu/uga/devdogs/course_information/Course/CourseRepository.java
@@ -11,8 +11,7 @@ public interface CourseRepository extends JpaRepository<Course, Long>{
     //This will get a course by its unique ID
     Course getById(Long id);
 
-    //@Query("select u from Course where u.title = ?1")
-    //Course getByTitle(String title);
+    Course getByTitle(String title);
 
     //This will get a list of Courses by their subject.
     List<Course> findBySubject(String subject);
