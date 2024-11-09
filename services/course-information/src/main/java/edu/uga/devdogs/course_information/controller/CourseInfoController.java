adac06264166e67406ba00bd13afcc102936f5fc
DogeDevYT <52612977+DogeDevYT@users.noreply.github.com>
2024-11-08 21:58:08 -0500

    Add in JPA Call logic for getCoursesByMajor() and halfway through getClassesByCrnAndTime()

--- services/course-information/src/main/java/edu/uga/devdogs/course_information/controller/CourseInfoController.java
+++ services/course-information/src/main/java/edu/uga/devdogs/course_information/controller/CourseInfoController.java
@@ -1,5 +1,6 @@
 package edu.uga.devdogs.course_information.controller;
 
+import edu.uga.devdogs.course_information.Course.Course;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
