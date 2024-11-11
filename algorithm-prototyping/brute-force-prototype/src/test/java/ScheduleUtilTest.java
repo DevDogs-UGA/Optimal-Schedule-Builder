2ac0e58bc7e21a0906857b3b660fd2d54b85765b
yousef-0614 <133291362+yousef-0614@users.noreply.github.com>
2024-11-11 18:12:17 -0500

    Finished testValidate().

--- algorithm-prototyping/brute-force-prototype/src/test/java/ScheduleUtilTest.java
+++ algorithm-prototyping/brute-force-prototype/src/test/java/ScheduleUtilTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import edu.uga.devdogs.bruteforceprototype.schedule.ScheduleUtil;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -29,6 +30,8 @@ public class ScheduleUtilTest {
         SampleData sampleData = SampleDataParser.parse(professorsFilePath, coursesFilePath, distancesFilePath);
         courses = sampleData.courses();
         distances = sampleData.distances();
+        validTestSchedules = new ArrayList<>();
+        invalidTestSchedules = new ArrayList<>();
 
         // Create valid test schedules
         validTestSchedules.add(new Schedule(Set.of(
