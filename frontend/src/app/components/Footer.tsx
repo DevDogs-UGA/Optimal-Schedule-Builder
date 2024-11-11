748c768d5303dc3b240c22370e6c35f8c8637cb8
hilton <delvalle.hilton@gmail.com>
2024-11-11 15:00:31 -0500

    Update Footer.tsx
    Pinned footer to the bottom of the page and ensured that it stays in front of other elements.

--- frontend/src/app/components/Footer.tsx
+++ frontend/src/app/components/Footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="mt-4 w-full bg-slate-200">
+    <footer className="fixed bottom-0 mt-4 w-full bg-slate-200 z-50">
       <div className="flex items-center justify-between px-8 py-4">
         <div className="flex flex-col gap-x-4 gap-y-2 sm:flex-row">
           <a href="" className="hover:text-slate-900">
