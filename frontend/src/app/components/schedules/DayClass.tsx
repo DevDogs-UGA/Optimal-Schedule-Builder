90d0b01a74a69fb05155f738bc78679bdb96cc0e
hilton <delvalle.hilton@gmail.com>
2024-11-15 09:50:00 -0500

    figuring out useState

--- frontend/src/app/components/schedules/DayClass.tsx
+++ frontend/src/app/components/schedules/DayClass.tsx
@@ -1,7 +1,18 @@
+"use client";
+
 import { type ClassData } from "../../../types/scheduleTypes";
+import { useState } from "react";
 
 interface DayClassProps extends ClassData {
   className?: string;
+}
+
+function CourseInfo() {
+  return (
+    <div className="place-content-center absolute top-full left-0 bg-white border border-gray-300 shadow-lg z-10 p-10">
+      insert class info here
+    </div>
+  );
 }
 
 export default function DayClass({
@@ -13,8 +24,15 @@ export default function DayClass({
   timeDifference,
   className = "",
 }: DayClassProps) {
+
+  const [courseBlockClicked, setcourseBlockClicked] = useState(false);
+
+  const courseBlockInfo = () => {
+    setcourseBlockClicked(!courseBlockClicked);
+  }
+
   return (
-    <div className={`relative ${className}`}>
+    <div className={`relative ${className}`} onClick={courseBlockInfo}>
       <div
         className={`w-full rounded-lg p-4 transition duration-150 ease-in-out hover:bg-black ${bgColor} flex justify-between`}
         style={{
@@ -31,6 +49,7 @@ export default function DayClass({
           <p>{timeEnd}</p>
         </div>
       </div>
+      {courseBlockClicked && <CourseInfo />}
     </div>
   );
 }
