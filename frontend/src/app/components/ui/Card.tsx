9cddb278d169d4ea384ae3ec2224b81e224b3ad3
garrett-plumlee <gap48709@uga.edu>
2024-11-15 11:27:50 -0500

    Card Height Adjusted for No Image

--- frontend/src/app/components/ui/Card.tsx
+++ frontend/src/app/components/ui/Card.tsx
@@ -15,9 +15,11 @@ export function Card({ image, heading, text, handleClick }: props) {
       <div className="h-1 w-full max-w-lg mx-auto bg-[#B3072A]"></div>
       <div className="relative flex w-full max-w-md flex-col items-center justify-center mx-auto bg-[#fffafa] bg-[url('/images/paws.svg')] p-8 pb-32 sm:max-w-lg sm:p-16 overflow-hidden">
         {/* Image */}
-        <div className="p-4 pt-0 sm:p-8">
-          <img className="min-h-24 sm:min-h-36" src={image} alt="Image" />
-        </div>
+        {image &&(
+          <div className="p-4 pt-0 sm:p-8">
+            <img className="min-h-24 sm:min-h-36" src={image} alt="Image" />
+          </div>
+        )}
         {/* Heading */}
         <div>
           <h1 className="mt-0 text-center text-2xl font-extrabold sm:mt-8">
--- frontend/src/app/test-page/page.tsx
+++ frontend/src/app/test-page/page.tsx
@@ -7,9 +7,9 @@ export default function Test() {
         <div className="">
             <Navbar />
             <div className="grid grid-grow grid-rows-8 grid-cols-2 span items-center justify-center pt-10 text-center">
-                <div className="h-[10rem]">
-                    <Card image="https://cdn.outsideonline.com/wp-content/uploads/2023/03/Funny_Dog_S.jpg" 
-                    text="This is our dawg his name is Fluffy. Fluffy likes to play fetch. We like Fluffy." heading="This is our dawg" />
+                <div className="">
+                    <Card image="" 
+                    text="Fluffy has left the server." heading="Where'd the dawg go?" />
                 </div>
                 <div className="">
                     <Card image="https://cdn.outsideonline.com/wp-content/uploads/2023/03/Funny_Dog_S.jpg" 
