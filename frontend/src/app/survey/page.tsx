044f03f3f94e7ec51fb7ebbd5d91826f954ddff1
Jeffrey Luo <77336910+lunarsceals@users.noreply.github.com>
2024-11-18 11:11:10 -0500

    Progress: working on questionnaire page and dropdown component

--- frontend/src/app/survey/page.tsx
+++ frontend/src/app/survey/page.tsx
@@ -1,61 +1,121 @@
 "use client";
-import Dropdown from "@/components/Dropdown";
-import { useState } from "react";
+import Dropdown from "../components/ui/DropDown";
 import { z } from "zod";
+import { useRef } from "react";
+import { Button } from "../components/ui/Button";
+import useForm from "../hooks/useForm";
 
 //TODO: Style the questionnaire page
 //TODO: Create handler to trigger zod schema errors on page
 
 //Schema created using zod to test validation in the form
-
 const surveySchema = z.object({
-  major: z.string().min(1, { message: "major is required" }),
-  distance: z
-    .string()
-    .regex(/^[^-]*$/, "distane must be positive")
-    .min(1, { message: "distance is requried" }),
-  semester: z.string().min(1, { message: "semester is required" }),
+  major: z.string().min(1, { message: "Major is required" }),
+  distance: z.string().min(1, { message: "Distance is requried" }),
+  hours: z.string().min(1, { message: "Credit Hours are requried" }),
+  semester: z.string().regex(/^(fall|stpring).*$/i, "Semester is required"),
 });
 
 export default function QuestionnarePage() {
-  function formAction(formData: FormData) {
-    const formValues = Object.fromEntries(formData);
-    try {
-      const result = surveySchema.parse(formValues);
-      const surveyData = JSON.stringify(result);
-      console.log(surveyData);
-    } catch (err) {
-      if (err instanceof z.ZodError) {
-        err.errors.forEach((error) => {
-          console.error(error.message);
-        });
-      }
-    }
-  }
+  const initialState = {
+    major: "",
+    distance: "",
+    hours: "",
+    semester: "",
+  };
 
-  const data = ["comptuer science", "mandarin", "biology"];
+  const { values, handleChange, resetForm } = useForm(initialState);
+  console.log(values);
+  // const formRef = useRef<HTMLFormElement>(null);
+  // function formAction(event: React.FormEvent<HTMLFormElement>) {
+  //   event.preventDefault();
+  //   if (formRef.current) {
+  //     const formData = new FormData(formRef.current);
+  //     const formValues = Object.fromEntries(formData);
+  //     try {
+  //       const result = surveySchema.parse(formValues);
+  //       // const surveyData = JSON.stringify(result);
+  //       console.log(result);
+  //     } catch (err) {
+  //       if (err instanceof z.ZodError) {
+  //         err.errors.forEach((error) => {
+  //           console.error(error.message);
+  //         });
+  //       }
+  //     }
+  //   }
+  // }
+
+  const dummyData = [
+    "Web Programming",
+    "Data Structures",
+    "Biology",
+    "Geology",
+    "Algorithms",
+    "Software Developement",
+    "For example a really long class title",
+  ];
+  const semesterList = [
+    `Fall ${new Date().getFullYear()}`,
+    `Spring ${new Date().getFullYear() + 1}`,
+  ];
+  const dropdownStyling =
+    "select-none rounded-md border-2 px-3 py-1 text-black focus:outline-none";
   return (
-    <main>
-      <form action={formAction}>
-        <Dropdown
-          items={data}
-          type="text"
-          name="major"
-          className="border-2 border-gray-600 text-black"
-        />
-        <Dropdown
-          type="number"
-          name="distance"
-          className="border-2 border-gray-600 text-black"
-        />
-        <Dropdown
-          type="text"
-          name="semester"
-          className="border-2 border-gray-600 text-black"
-        />
-        <br />
-        <button>Submit</button>
-      </form>
-    </main>
+    <div className="flex h-screen items-center justify-center">
+      <main>
+        <form
+          // ref={formRef}
+          // onSubmit={formAction}
+          className="flex flex-col gap-6 rounded-2xl bg-dev-dog-blue p-12 text-center"
+        >
+          <div>
+            <h1>What&apos;s your major?</h1>
+            <Dropdown
+              items={dummyData}
+              type="text"
+              name="major"
+              className={dropdownStyling}
+              placeholder="Enter your major"
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <h1>Distance between classes?</h1>
+            <Dropdown
+              type="number"
+              name="distance"
+              className={dropdownStyling}
+              placeholder="Enter distance to class"
+              onChange={handleChange}
+              min="0"
+            />
+          </div>
+          <div>
+            <h1>How many credit hours?</h1>
+            <Dropdown
+              type="number"
+              name="hours"
+              className={dropdownStyling}
+              placeholder="Enter credit hours"
+              onChange={handleChange}
+              min="0"
+            />
+          </div>
+          <div>
+            <h1>Which semester?</h1>
+            <Dropdown
+              items={semesterList}
+              type="text"
+              name="semester"
+              className={dropdownStyling}
+              placeholder="Enter semester"
+              onChange={handleChange}
+            />
+          </div>
+          <Button text="Submit" className="text-white" />
+        </form>
+      </main>
+    </div>
   );
 }
