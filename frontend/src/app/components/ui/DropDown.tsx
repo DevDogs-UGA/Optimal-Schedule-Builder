a564404cc5d765364017629a88193e8bd31497fa
Jeffrey Luo <77336910+lunarsceals@users.noreply.github.com>
2024-11-22 10:01:02 -0500

    Finish Questionnaire Page & Dropdown component + Create DropdownTagInput component

--- frontend/src/app/components/ui/DropDown.tsx
+++ frontend/src/app/components/ui/DropDown.tsx
@@ -1,91 +1,125 @@
 "use client";
 
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState, useRef, forwardRef } from "react";
+import { type z } from "zod";
 
 interface DropdownProps {
   items?: string[];
   type?: string;
   name?: string;
-  className?: string;
   min?: string;
+  className?: string;
+  isFormSubmitted?: boolean;
+  errorList?: z.ZodIssue[];
   placeholder?: string;
-  formStatus?: boolean;
+  readOnly?: boolean;
 }
 
-export default function Dropdown({
-  items = [],
-  type = "text",
-  name,
-  className,
-  min,
-  placeholder,
-  formStatus,
-}: DropdownProps) {
-  const [isOpen, setIsOpen] = useState(false);
-  const [query, setQuery] = useState("");
-  const [filteredData, setFilteredData] = useState<string[]>(items);
-  const dropdownRef = useRef<HTMLUListElement>(null);
+//Uses forwardRef to get the input field props to the parent
+export const Dropdown = forwardRef<HTMLInputElement, DropdownProps>(
+  (
+    {
+      items = [],
+      type = "text",
+      name,
+      min,
+      className,
+      isFormSubmitted,
+      errorList,
+      placeholder,
+      readOnly,
+    },
+    ref,
+  ) => {
+    //Handles Dropdown
+    const [isOpen, setIsOpen] = useState(false);
+    const [query, setQuery] = useState("");
+    const [filteredData, setFilteredData] = useState<string[]>(items);
+    const dropdownRef = useRef<HTMLUListElement>(null);
 
-  useEffect(() => {
-    const handleClose = (e: MouseEvent) => {
-      if (!dropdownRef.current?.contains(e.target as Node)) {
-        setIsOpen(false);
+    //Handles errors
+    const [error, setError] = useState<z.ZodIssue>();
+
+    //Handles closing dropdown menu when clicking off of it
+    useEffect(() => {
+      const handleClose = (e: MouseEvent) => {
+        if (!dropdownRef.current?.contains(e.target as Node)) {
+          setIsOpen(false);
+        }
+      };
+      document.addEventListener("mousedown", handleClose);
+      return () => {
+        document.removeEventListener("mousedown", handleClose);
+      };
+    }, [isOpen]);
+
+    //Filters dropdown data using the current search query with regex
+    const handleQuery = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const regex = new RegExp("^" + e.currentTarget.value, "i");
+      const filter = items.filter((item) => regex.test(item));
+      setFilteredData(filter);
+    };
+
+    //Handles clearing inputs
+    useEffect(() => {
+      setQuery("");
+    }, [isFormSubmitted]);
+
+    useEffect(() => {
+      if (errorList) {
+        setError(errorList.find((err) => err.path[0] === name));
       }
-    };
-    document.addEventListener("mousedown", handleClose);
-    return () => {
-      document.removeEventListener("mousedown", handleClose);
-    };
-  }, [isOpen]);
+    }, [errorList]);
 
-  const handleQuery = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const regex = new RegExp("^" + event.currentTarget.value, "i");
-    const filter = items.filter((item) => regex.test(item));
-    setFilteredData(filter);
-  };
+    const sharedStyling = "w-full rounded-md border-2 ";
 
-  useEffect(() => {
-    setQuery("");
-  }, [formStatus]);
-
-  return (
-    <>
-      <input
-        id="input"
-        onClick={() => setIsOpen(true)}
-        onInput={(e) => {
-          setIsOpen(true);
-          setQuery(e.currentTarget.value);
-        }}
-        onChange={handleQuery}
-        value={query}
-        type={type}
-        name={name}
-        className={`border-2 px-2 py-1 focus:outline-none ${className}`}
-        min={min}
-        placeholder={placeholder}
-        autoComplete="off"
-      />
-
-      {isOpen && filteredData.length > 0 && (
-        <ul
-          ref={dropdownRef}
-          className={`absolute bg-white text-black ${className} max-h-44 overflow-y-scroll`}
-        >
-          {filteredData.map((item, index) => (
-            <li
-              className="p-1 text-left hover:bg-limestone"
-              onClick={(e) => {
-                setQuery(e.currentTarget.textContent ?? "");
-                setIsOpen(false);
-              }}
-              key={index}
-            >
-              {item}
-            </li>
-          ))}
-        </ul>
-      )}
-    </>
-  );
-}
+    return (
+      <div className="relative">
+        <input
+          ref={ref}
+          id="input"
+          onClick={() => setIsOpen(true)}
+          onInput={(e) => {
+            setIsOpen(true);
+            setQuery(e.currentTarget.value);
+            setError(undefined);
+          }}
+          onChange={handleQuery}
+          value={query}
+          type={type}
+          name={name}
+          className={`h-12 px-2 py-1 focus:outline-none ${sharedStyling} ${error ? "border-red-600" : " "} ${className}`}
+          min={min}
+          placeholder={placeholder}
+          autoComplete="off"
+          readOnly={readOnly}
+        />
+        {error && (
+          <div className="absolute font-thin text-bulldog-red">
+            {"* " + error.message}
+          </div>
+        )}
+        {isOpen && filteredData.length > 0 && (
+          <ul
+            ref={dropdownRef}
+            className={`absolute z-50 max-h-44 overflow-y-scroll bg-white text-black ${sharedStyling} ${className} scroll-smooth`}
+          >
+            {filteredData.map((item, index) => (
+              <li
+                className="px-2 py-1 text-left transition ease-in-out hover:bg-limestone"
+                onClick={(e) => {
+                  setQuery(e.currentTarget.textContent ?? "");
+                  setIsOpen(false);
+                  setError(undefined);
+                }}
+                key={index}
+              >
+                {item}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    );
+  },
+);
