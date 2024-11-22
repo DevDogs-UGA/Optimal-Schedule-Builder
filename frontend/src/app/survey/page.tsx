73b53f2baf2c368fcea72904f7c71d5aca0eb6d8
Jeffrey Luo <77336910+lunarsceals@users.noreply.github.com>
2024-11-22 10:47:38 -0500

    Fix sizing issues

--- frontend/src/app/survey/page.tsx
+++ frontend/src/app/survey/page.tsx
@@ -7,8 +7,8 @@ export default function page() {
     <>
       <Navbar />
       <div className="min-w-screen flex min-h-screen items-center justify-center">
-        <main className="flex flex-col border-dusty-pink sm:mx-8 sm:flex-row sm:overflow-hidden sm:rounded-xl sm:border-4 sm:shadow-2xl">
-          <div className="flex items-center justify-center text-wrap border-4 border-b-0 border-barely-pink bg-white p-5 text-6xl font-bold sm:border-0 sm:bg-barely-pink sm:pb-[30%] sm:pl-14 md:w-1/2">
+        <main className="b-30 flex flex-col border-dusty-pink sm:flex-row sm:overflow-hidden sm:rounded-xl sm:border-4 sm:shadow-2xl md:m-20">
+          <div className="flex items-center justify-center text-wrap border-4 border-b-0 border-barely-pink bg-white text-5xl font-bold sm:border-0 sm:bg-barely-pink sm:pb-[30%] md:w-1/2">
             <div className="flex flex-col">
               <h1 className="sm:text-nearly-black">
                 Let&apos;s Get You <br /> Started!
