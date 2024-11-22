5c95d76577ef8ead4a0e5add3c8413bd755ec6f1
Jeffrey Luo <77336910+lunarsceals@users.noreply.github.com>
2024-11-22 11:09:54 -0500

    Fix Styling Issues

--- frontend/src/app/survey/page.tsx
+++ frontend/src/app/survey/page.tsx
@@ -7,7 +7,7 @@ export default function page() {
     <>
       <Navbar />
       <div className="min-w-screen flex min-h-screen items-center justify-center">
-        <main className="flex flex-col border-dusty-pink sm:mx-8 sm:flex-row sm:overflow-hidden sm:rounded-xl sm:border-4 sm:shadow-2xl">
+        <main className="flex flex-col justify-center border-dusty-pink sm:mx-8 sm:flex-row sm:overflow-hidden sm:rounded-xl sm:border-4 sm:shadow-2xl">
           <div className="flex items-center justify-center text-wrap border-4 border-b-0 border-barely-pink bg-white p-5 text-6xl font-bold sm:border-0 sm:bg-barely-pink sm:pb-[30%] sm:pl-14 md:w-1/2">
             <div className="flex flex-col">
               <h1 className="sm:text-nearly-black">
