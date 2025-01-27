497c327db810e5e4f2256bdb98c38feb4ff42410
SFNDude123502 <102396379+SFNDude123502@users.noreply.github.com>
2025-01-27 00:02:59 -0500

    Filled in dataPreFilter (#435)
    * Added inputCourse size check to dataPreFilter
    Changed dataPreFilter method signature to indicate that it may throw an exception.
    Added simplistic exception handling in generateValidSchedules and optimize
    Updated Javadocs for dataPreFilter and optimize to include info about exceptions / error handling
    
    * Updated Javadocs for generateValidSchedules to include info about exceptions / error handling

--- algorithm-prototyping/brute-force-prototype/src/main/java/edu/uga/devdogs/bruteforceprototype/BruteForcePrototype.java
+++ algorithm-prototyping/brute-force-prototype/src/main/java/edu/uga/devdogs/bruteforceprototype/BruteForcePrototype.java
@@ -13,11 +13,14 @@ public class BruteForcePrototype {
     /**
      * Cleans the data being sent into the algorithm to account for certain hard constraints.
      *
-     * @param inputCourses a set of courses given to be used in the user's algorithm.
+     * @param inputCourses a set of courses given to be used in the user's schedule.
+     * @throws Exception if {@code inputCourses} is not compliant with the filter.
      * @return a set of courses cleaned per the user's specification
      */
-    public static Set<Course> dataPreFilter(Set<Course> inputCourses) {
-
+    public static Set<Course> dataPreFilter(Set<Course> inputCourses) throws Exception {
+        if (inputCourses.size() > 10){
+            throw new Exception("Input course list contains more than ten courses.");
+        }
 
         return inputCourses;
     }
@@ -30,6 +33,8 @@ public class BruteForcePrototype {
      * {@code ScheduleUtil.computeOverallObjective(schedule, distances, weights)},
      * and returns the first five items of the list.
      *
+     * If the courses inputted do not pass the dataPreFilter, this function will return null.
+     *
      * @param inputCourses a set of courses to generate an optimal schedule from
      * @param distances a nested string map that represents distances between buildings on campus
      * @param weights an array of floats representing the weights for each objective
@@ -37,6 +42,18 @@ public class BruteForcePrototype {
      */
     public static List<Schedule> optimize(Set<Course> inputCourses, Map<String, Map<String, Double>> distances,  double[] weights) {
         Set<Schedule> validSchedules = generateValidSchedules(inputCourses);
+
+        if (validSchedules == null) {
+            // If validSchedules is null, it means that there was an exception from dataPreFilter;
+            // The error message would be already printed in generateValidSchedules,
+            // so we just need to return null here to indicate that the input data was invalid
+            return null;
+        }else if (validSchedules.isEmpty()){
+            // This block can be expanded in the future to include other methods of handling a lack of
+            // Hard-constraint compliant schedules.
+            System.out.println("No valid schedules found.");
+            return new ArrayList<>();
+        }
 
         List<Schedule> sortedSchedules = new ArrayList<>();
         List<Double> sortedOverallObjectives = new ArrayList<>();
@@ -66,11 +83,22 @@ public class BruteForcePrototype {
      * schedules with time conflicts.
      * Uses helper function {@code recurGenerateValidSchedules()} to generate schedules and record them
      *
+     * If the courses inputted do not pass the dataPreFilter, this function will return null.
+     *
      * @param inputCourses the set of courses to generate schedules from
      * @return the set of unique, valid schedules for the given set of courses
      */
     public static Set<Schedule> generateValidSchedules(Set<Course> inputCourses) {
-        List<Course> courseList = new ArrayList<>(dataPreFilter(inputCourses));
+        List<Course> courseList;
+
+        try{
+            courseList = new ArrayList<>(dataPreFilter(inputCourses));
+        } catch (Exception e){
+            // Boilerplate error handling; We can probably decide on a better method for handling errors later.
+            System.out.println(e.getMessage());
+            return null;
+        }
+
         Set<Section> sectionList = new HashSet<>();
         HashSet<Schedule> validSchedules = new HashSet<>();
 
