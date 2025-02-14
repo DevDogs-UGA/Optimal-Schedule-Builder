2f3ef651fb486580c025a089fb026dd7513f8637
garrett-plumlee <131043394+garrett-plumlee@users.noreply.github.com>
2025-02-14 09:58:42 -0500

    Revert "Corrected Homepage bug with scrolling"

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,7 +2,6 @@ import "@/styles/globals.css";
 import { Heebo } from "next/font/google";
 import { type Metadata } from "next";
 import { Footer } from "@/components/Footer";
-import Image from "next/image"; // Ensure this is imported
 
 const heebo = Heebo({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -18,32 +17,8 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={heebo.variable}>
-      <body className="relative flex min-h-screen flex-col bg-background">
-        <Image
-          className="absolute left-0 top-0 -z-10"
-          src="/images/paws.svg"
-          alt="Paws"
-          width={300}
-          height={300}
-        />
-        <Image
-          className="absolute bottom-0 right-0 -z-10"
-          src="/images/paws.svg"
-          alt="Paws"
-          width={300}
-          height={300}
-        />
-        <Image
-          className="absolute bottom-1/3 right-1/4 -z-10 hidden lg:block"
-          src="/images/paws.svg"
-          alt="Paws"
-          width={300}
-          height={300}
-        />
-
-        <main className="relative flex flex-1 flex-col">
-          <div className="mb-[18rem]">{children}</div>
-        </main>
+      <body className="flex min-h-screen flex-col">
+        <main className="relative flex flex-1 flex-col">{children}</main>
         <Footer />
       </body>
     </html>
