766f51a7af158386c74990015512405a15e85dd2
MustafaK4 <114113773+Mustafa-K4@users.noreply.github.com>
2025-02-17 18:52:06 -0500

    Updated Semester Dropdown
    adjusted parameters for edge cases

--- frontend/src/app/components/survey/QuestionnaireForm.tsx
+++ frontend/src/app/components/survey/QuestionnaireForm.tsx
@@ -119,26 +119,25 @@ export const QuestionnareForm = ({ className }: Props) => {
     `Summer ${new Date().getFullYear() + 1}`,
   ];
   
-
-
   //This will change the semester list based on the current month
-  // For Janurary: (Srping of this year, Summer of this year, Fall of this year)
-  if ((new Date().getMonth() < 1)) {
+  // For Janurary: (Spring of this year, Summer of this year, Fall of this year)
+  if ((new Date().getMonth() == 0)) {
     semesterList[0] = `Spring ${new Date().getFullYear()}`;
     semesterList[1] = `Summer ${new Date().getFullYear()}`;
     semesterList[2] = `Fall ${new Date().getFullYear()}`;
 
-  // For Feburary - June: (Summer of this year, Fall of this year, Spring of next year)
-  } else if (new Date().getMonth() < 6 && new Date().getMonth() < 8) {
+  // For Feburary - May: (Summer of this year, Fall of this year, Spring of next year)
+  } else if (new Date().getMonth() < 5) {
     semesterList[0] = `Summer ${new Date().getFullYear()}`;
     semesterList[1] = `Fall ${new Date().getFullYear()}`;
     semesterList[2] = `Spring ${new Date().getFullYear() + 1}`;
 
-  // For August - December: (Spring of next year, Summer of next year, Fall of next year)
-  } else if (new Date().getMonth() > 8) {
+  // For September - December: (Spring of next year, Summer of next year, Fall of next year)
+  } else if (new Date().getMonth() > 7) {
     semesterList[0] = `Spring ${new Date().getFullYear() +1}`;
     semesterList[1] = `Summer ${new Date().getFullYear()+1}`;
     semesterList[2] = `Fall ${new Date().getFullYear()+1}`;
+    //alert(`the month is ${testDay.getMonth()}`);
   }
 
   const creditHoursList = Array.from({ length: 18 }, (_, i) =>
