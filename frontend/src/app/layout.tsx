ee7a85f5f31275e634574a9a3aede1dee8970748
Sloan Finger <hello@sloanfinger.com>
2025-02-17 23:10:43 -0500

    fix bugs, typos

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,8 +1,9 @@
-import "@/styles/globals.css";
-import { Heebo } from "next/font/google";
-import { type Metadata } from "next";
 import { Footer } from "@/components/Footer";
+import { QueryClientProvider } from "@/hooks/useQuery";
 import { ToastProvider } from "@/hooks/useToast";
+import "@/styles/globals.css";
+import { type Metadata } from "next";
+import { Heebo } from "next/font/google";
 
 const heebo = Heebo({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -19,10 +20,12 @@ export default function RootLayout({
   return (
     <html lang="en" className={heebo.variable}>
       <body className="flex min-h-screen flex-col">
-        <ToastProvider>
-          <main className="relative flex flex-1 flex-col">{children}</main>
-          <Footer />
-        </ToastProvider>
+        <QueryClientProvider>
+          <ToastProvider>
+            <main className="relative flex flex-1 flex-col">{children}</main>
+            <Footer />
+          </ToastProvider>
+        </QueryClientProvider>
       </body>
     </html>
   );
--- frontend/src/hooks/useQuery.tsx
+++ frontend/src/hooks/useQuery.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import serverQuerySchema from "@/schemas/serverQueries";
 import {
+  QueryClient as ReactQueryClient,
+  QueryClientProvider as ReactQueryClientProvider,
   type UseQueryOptions,
   useQuery as useReactQuery,
 } from "@tanstack/react-query";
@@ -17,19 +21,22 @@ function createUseQuery<
 >(queries: QueryRecord) {
   /**
    * This hook is a wrapper for react-query which provides typed
-   * parameters and responses for the course information backend 
+   * parameters and responses for the course information backend
    * service. These types are defined in `@schema/serverQueries`.
-   * 
+   *
    * @param queryKey The query to call on the backend.
    * @param params The parameters associated with the provided query.
    * @param options Options passed through to the underlying react-query hook.
-   * 
+   *
    * @see https://tanstack.com/query/latest
    */
   return function useQuery<Q extends keyof QueryRecord>(
     queryKey: Q,
     params: z.input<QueryRecord[Q]["params"]>,
-    options: Omit<UseQueryOptions<z.output<QueryRecord[Q]["result"]>>, "queryKey" | "queryFn"> = {}
+    options: Omit<
+      UseQueryOptions<z.output<QueryRecord[Q]["result"]>>,
+      "queryKey" | "queryFn"
+    > = {},
   ) {
     const query = queries[queryKey];
 
@@ -48,7 +55,7 @@ function createUseQuery<
                  * information service, we'll store the URL in an
                  * environment variable, and it will be accessible here.
                  */
-                // env.COURSE_INFORMATION_SERVICE,
+                // env.NEXT_PUBLIC_COURSE_INFORMATION_SERVICE
               ),
             ),
           )
@@ -59,4 +66,14 @@ function createUseQuery<
 }
 
 const useQuery = createUseQuery(serverQuerySchema);
+const queryClient = new ReactQueryClient();
+
 export default useQuery;
+
+export function QueryClientProvider({ children }: React.PropsWithChildren) {
+  return (
+    <ReactQueryClientProvider client={queryClient}>
+      {children}
+    </ReactQueryClientProvider>
+  );
+}
