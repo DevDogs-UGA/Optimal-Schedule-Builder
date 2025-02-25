5ff81a1a5d4976e2a16b4168c64aa932fae0a8e3
SidhantDash <sidhantdash126@gmail.com>
2025-02-24 23:57:00 -0500

    Update AddCourses.tsx

--- frontend/src/components/courses/AddCourses.tsx
+++ frontend/src/components/courses/AddCourses.tsx
@@ -35,7 +35,7 @@ export const AddCourses = () => {
   //TODO: Replace all dummy data with their respective items
   // Replace '[]' with "subjectsDummyData" to test dropdown
   const subjectData: string[] | undefined = [];
-  subjectData ?? querySubjects.data;
+  subjectData ?? (querySubjects.data);
   const courseData: string[] = [];
   const instructorData: string[] = [];
   const CRNData: string[] = [];
