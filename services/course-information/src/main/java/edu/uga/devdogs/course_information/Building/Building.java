3379f76d029180c03c8f230d9f497e9ab9767a13
KadeStyron <kjs00988@uga.edu>
2025-03-17 20:57:25 -0400

    Revert "Merge pull request #642 from akira23456/dev"
    This reverts commit a30fa02239ac4771d04b21772c0aa2db791d0b39, reversing
    changes made to 9c441ea1bc660d8014bba8041bda3ee04b0a3d5e.

--- services/course-information/src/main/java/edu/uga/devdogs/course_information/Building/Building.java
+++ services/course-information/src/main/java/edu/uga/devdogs/course_information/Building/Building.java
@@ -1,45 +1,78 @@
 package edu.uga.devdogs.course_information.Building;
 
 import edu.uga.devdogs.course_information.Class.ClassEntity;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
 import java.io.Serializable;
 import java.util.List;
 
 /*
- * Java JPA entity representation for Building
+ * Java JPA entity represention for Building
  */
 @Entity
 public class Building implements Serializable {
     
+    /*
+     * Variables
+     */
+
+    // serves as the id.
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long buildingId;
 
+    /* This can't serve as the id, because many 
+    classes show the building number as NCRR or TBA/
+    */
     private String buildingNumber;
-    private String name;
-    private String grid;
-    private double latitude;
-    private double longitude;
 
+    private String name;
+
+    private String grid;
+
+    /*
+     * Relationships
+     * To-Do: add relationships (one-to-one, one-to-many, many-to-one, many-to-many) here
+     */
     @OneToMany(mappedBy = "building")
     private List<ClassEntity> classes;
 
-    public Building() {}
+    /*
+     * Constructors
+     */
 
-    public Building(String buildingNumber, String name, String grid, double latitude, double longitude) {
+    // Default constructor
+    public Building() {
+    }
+
+    // Constructor w/o buildingNumber
+    public Building(String name, String grid) {
+        this.name = name;
+        this.grid = grid;
+    }
+
+    // Constructor w/ buildingNumber
+    public Building(String buildingNumber, String name, String grid) {
         this.buildingNumber = buildingNumber;
         this.name = name;
         this.grid = grid;
-        this.latitude = latitude;
-        this.longitude = longitude;
+    }
+
+    
+    /*
+     * Getters and Setters
+     */
+
+    public String getBuildingNumber() {
+        return buildingNumber;
     }
 
     public long getBuildingId() {
         return buildingId;
-    }
-
-    public String getBuildingNumber() {
-        return buildingNumber;
     }
 
     public void setBuildingNumber(String buildingNumber) {
@@ -62,22 +95,6 @@ public class Building implements Serializable {
         this.grid = grid;
     }
 
-    public double getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
-
-    public double getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
-    }
-
     public List<ClassEntity> getClasses() {
         return classes;
     }
@@ -86,12 +103,16 @@ public class Building implements Serializable {
         this.classes = classes;
     }
 
+    /*
+     * toString
+     */
     @Override
     public String toString() {
-        return "Building [buildingNumber=" + buildingNumber 
+        return "Building [ buildingNumber=" + buildingNumber 
                 + ", name='" + name 
-                + "', grid='" + grid 
-                + "', latitude=" + latitude 
-                + ", longitude=" + longitude + "]";
+                + ", grid='" + grid 
+                + ']';
     }
+
+    
 }
