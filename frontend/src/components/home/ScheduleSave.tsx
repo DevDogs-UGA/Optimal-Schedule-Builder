f86b1e756797f8530529a37f3f85eb9fa71fef6e
Aiden Mori-Lair <167143359+akira23456@users.noreply.github.com>
2025-03-24 18:14:33 -0400

    CourseHours conversion to an int

--- frontend/src/components/home/ScheduleSave.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function ScheduleSave() {
-  return (
-    <div className="flex h-24 w-24 items-center justify-center rounded-xl bg-[#F3EDED] transition duration-300 ease-in-out hover:bg-[#c3a5a5] sm:h-32 sm:w-32 md:h-40 md:w-40">
-      <p className="text-[10px] sm:text-[14px] md:text-[16px]">Schedule Save</p>
-    </div>
-  );
-}
--- services/bulletin/src/main/java/edu/uga/devdogs/bulletin/exceptions/SectionNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package edu.uga.devdogs.bulletin.exceptions;
-
-public class SectionNotFoundException extends RuntimeException {
-    public SectionNotFoundException(String message) {
-        super(message);
-    }
-}
