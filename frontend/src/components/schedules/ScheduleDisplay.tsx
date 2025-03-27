abcdf9aa699270d7bd66292fdd6c04682061906d
hilton326 <hjd59728@uga.edu>
2025-03-27 18:46:58 -0500

    Schedule Pinning Implemented

--- frontend/src/components/schedules/ScheduleDisplay.tsx
+++ frontend/src/components/schedules/ScheduleDisplay.tsx
@@ -6,16 +6,15 @@ import WeekSchedule from "@/components/schedules/WeekSchedule";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 import { useState, useEffect } from "react";
-import { PiHeart } from "react-icons/pi";
-import { PiArrowLeft } from "react-icons/pi";
-import { PiArrowRight } from "react-icons/pi";
-import { PiX } from "react-icons/pi";
+import { PiArrowLeft, PiArrowRight, PiX } from "react-icons/pi";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 
 export default function ScheduleDisplay({ bgColors }: { bgColors: string[] }) {
   // Map colors to course (if the course includes a lab, drop the L from the string)
   const colorMapping: Record<string, string> = {};
   const usedColors: Set<string> = new Set<string>();
 
+  // I feel like this function needs to go somewhere else
   function getBgColorForClass(classTitle: string): string {
     // Course block colors:
     // Check if color has already been used
@@ -55,24 +54,30 @@ export default function ScheduleDisplay({ bgColors }: { bgColors: string[] }) {
 
   // Retrieve the current schedule data and title (from the SavedPlan component)
   const searchParams = useSearchParams();
-  const savedPlan = searchParams.get("data");
-  const planTitle = searchParams.get("title");
+  const savedPlan = searchParams.get("data") ?? "";
+  const planTitle = searchParams.get("title") ?? "";
+  const isPinned = searchParams.get("pinned") ?? "";
 
   // Parameters for a saved schedule
   interface SavedPlanType {
     title: string;
     data: WeekScheduleType;
+    pinned: boolean;
   }
 
   // List of the user's saved schedules
   const [savedPlans, setSavedPlans] = useState<SavedPlanType[]>([]);
+  
   // Used to enable selection of any schedule from the list
   const [currentPlanIndex, setCurrentPlanIndex] = useState(0);
+  // Used to enable pinning saved plans even on the schedule page
+  const planToPin = useState<string | null>(null);
+
 
   useEffect(() => {
     try {
       // Retrieve schedules from local storage and populate the plans array
-      const plans = [];
+      const plans: SavedPlanType[] = [];
       // Loop through local storage
       for (let i = 0; i < localStorage.length; i++) {
         const key = localStorage.key(i);
@@ -80,43 +85,40 @@ export default function ScheduleDisplay({ bgColors }: { bgColors: string[] }) {
         if (key) {
           // Retrieve data matching key
           const data = localStorage.getItem(key);
-          // Only proceed if the data looks like a WeekSchedule component
-          if (data?.startsWith('{"Monday"')) {
-            // Parse the data from the JSON
-            plans.push({
-              title: key,
-              data: JSON.parse(data) as WeekScheduleType,
-            });
+          if (data) {
+            const parsedData = JSON.parse(data) as SavedPlanType;
+            if (parsedData.data && parsedData.pinned !== undefined) {
+              // Parse the data from the JSON
+              plans.push({
+                title: key,
+                data: parsedData.data,
+                pinned: parsedData.pinned,
+              });
+            }
           }
         }
       }
-      // Sort plans by title
-      plans.sort((a, b) => a.title.localeCompare(b.title));
+      // Sort plans by title, and then again by pinned status
+      plans.sort((a, b) => Number(b.pinned) - Number(a.pinned) || a.title.localeCompare(b.title));
+
       // savedPlans = plans. Increases the scope of the plans array
       setSavedPlans(plans);
 
-      // Set initial current plan based on the SavedPlan arguments
-      if (savedPlan && planTitle) {
-        const initialPlan = {
-          title: planTitle,
-          data: JSON.parse(savedPlan) as WeekScheduleType,
-        };
-        // Get the index of the current plan and update the current plan index
-        const initialIndex = plans.findIndex(
-          (plan) => plan.title === planTitle,
-        );
-        setCurrentPlanIndex(initialIndex !== -1 ? initialIndex : 0);
-      }
+      // Get the index of the current plan and update the current plan index (trim any whitespace)
+      const initialIndex = plans.findIndex((plan) => plan.title.trim() === planTitle.trim());
+      setCurrentPlanIndex(initialIndex !== -1 ? initialIndex : 0);
+      
     } catch (error) {
       console.error("Failed to parse local storage data: ", error);
     }
-  }, [savedPlan, planTitle]);
+  }, [savedPlan, planTitle, isPinned]);
 
   /* The current plan/schedule is what is displayed on the page.
    * For safety, an empty schedule is assigned if the data from the array is null or cannot be read.
    */
   const currentPlan = savedPlans[currentPlanIndex] ?? {
-    title: "",
+    title: "Schedule",
+    pinned: false,
     data: { Monday: [], Tuesday: [], Wednesday: [], Thursday: [], Friday: [] },
   };
 
@@ -132,6 +134,37 @@ export default function ScheduleDisplay({ bgColors }: { bgColors: string[] }) {
     if (currentPlanIndex < savedPlans.length - 1) {
       setCurrentPlanIndex(currentPlanIndex + 1);
     }
+  };
+
+  // When the heart icon is clicked, the schedule will be considered "pinned" when returning to the plans list page
+  const pinPlan = () => {
+    if (currentPlan.title) {
+      setSavedPlans((prevPlans) => {
+        const updatedPlans = prevPlans.map((plan => {
+          if (plan.title === currentPlan.title) {
+            // Toggle the pinned status
+            const updatedPlan = { ...plan, pinned: !plan.pinned };
+            // Save the updated plan back to local storage
+            localStorage.setItem(currentPlan.title, JSON.stringify({ data: plan.data, pinned: updatedPlan.pinned }));
+            return updatedPlan;
+          }
+          return plan;
+        }));
+
+        // Sort plans so that pinned plans come first
+        updatedPlans.sort((a,b) => Number(b.pinned) - Number(a.pinned));
+
+        // Save sorted plans back to local storage
+        updatedPlans.forEach((plan => {
+          localStorage.setItem(plan.title, JSON.stringify({ data: plan.data, pinned: plan.pinned }));
+        }));
+
+        // Update the current plan index to reflect the new order
+        const newIndex = updatedPlans.findIndex((plan => plan.title === currentPlan.title));
+        setCurrentPlanIndex(newIndex);
+        return updatedPlans;
+      });
+    };
   };
 
   // Sorts the classes on the schedule display by start time
@@ -164,7 +197,13 @@ export default function ScheduleDisplay({ bgColors }: { bgColors: string[] }) {
           </div>
           {/* Save/exit buttons: exit returns to saved plan list */}
           <div className="flex flex-row">
-            <PiHeart size={32} className="mr-3 mt-5 fill-bulldog-red" />
+            <div onClick={pinPlan}>
+              {currentPlan.pinned ? (
+                <FaHeart size={30} className="mr-3 mt-5 fill-glory-glory-red transition" />
+              ) : (
+                <FaRegHeart size={30} className="mr-3 mt-5 transition hover:fill-glory-glory-red" />
+              )}
+            </div>            
             <Link href={"/saved-plans"}>
               <PiX size={32} className="mr-3 mt-5 hover:fill-bulldog-red" />
             </Link>
