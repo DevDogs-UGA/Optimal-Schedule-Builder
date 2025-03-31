7b778cdbcda229aa60ff8cbb95b41b7b9fefe3ec
Thai Nguyen <ttn64681@uga.edu>
2025-03-31 08:00:24 -0400

    Navbar and Footer (MVP Version) #670
    Navbar and Footer (MVP Version) #670
    
    I!!!!!   have a slight issue on not being able to get the middle content area smaller than what it currently is.  !!!!!!
    
    May need future revisement.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,62 +7,59 @@ import { Navbar } from "@/components/Navbar";
 import Image from "next/image";
 import Link from "next/link";
 import background from "../../public/images/background.png";
-import devdog from "../../public/images/devdog.png";
+import devdogCobranded from "../../public/images/devdogCobranded.png";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <div className="relative -mt-[3.625rem] flex flex-1 flex-col items-center justify-center gap-16 px-4 pt-24 text-center">
-        <Image
-          alt=""
-          className="absolute top-14 -z-10 max-h-full w-screen object-cover sm:top-0"
-          sizes="100vw"
-          src={background}
-        />
-        <Image
-          alt=""
-          className="absolute -bottom-36 -z-10 w-screen rotate-180 sm:hidden"
-          sizes="100vw"
-          src={background}
-        />
 
-        <div className="flex flex-col items-center gap-20 rounded-md bg-gradient-to-bl from-white/50 to-pink-100/50 px-6 py-16 backdrop-blur-sm min-[480px]:px-12 sm:px-16 md:contents">
-          <div className="flex gap-6 text-6xl md:text-7xl xl:text-8xl">
-            <h2 className="font-extrabold text-slate-800">DogDays</h2>
-            <figure className="size-[1em]">
-              <Image
-                alt="UGA Dev Dogs logo"
-                className=""
-                height={128}
-                src={devdog}
-              />
-            </figure>
-          </div>
+        <div className="flex flex-col items-center px-6 py-8 min-[480px]:px-12 sm:px-16 md:max-w-[80%] lg:max-w-[60%]">
+          <div className="flex flex-col items-center w-full">
+            <div className="flex justify-center text-6xl md:text-7xl xl:text-8xl">
+              <h2 className="font-extrabold text-slate-800">Bulldog Planner</h2>
+            </div>
 
-          <div className="flex flex-col gap-8 rounded-md from-white/50 to-pink-100/50 md:bg-gradient-to-bl md:px-16 md:py-8 md:backdrop-blur-sm">
-            <p className="flex flex-col font-bold text-bulldog-red">
-              <span className="text-xl sm:text-2xl xl:text-3xl">
-                An Optimized Schedule Builder
-              </span>
-              <span className="sm:text-lg xl:text-2xl">
-                For Students, By Students
-              </span>
-            </p>
+            <div className="w-full flex flex-col items-center">
+              <div className="flex items-center justify-center gap-8 w-full">
+                <div className="flex flex-col items-start">
+                  <div className="flex items-start">
+                    <div className="border-l-4 border-bulldog-red h-20 mr-4"></div>
+                    <div className="flex flex-col">
+                      <span className="text-2xl sm:text-3xl xl:text-4xl font-bold text-bulldog-red leading-tight text-left">
+                        An Optimized<br />
+                        Schedule Builder
+                      </span>
+                    </div>
+                  </div>
+                  <span className="text-left sm:text-lg xl:text-2xl font-medium text-bulldog-red mt-5">
+                    For Students, By Students
+                  </span>
+                </div>
+                <Image 
+                  alt="UGA Dev Dogs logo" 
+                  className="flex-shrink-0" 
+                  height={220} 
+                  width={220} 
+                  src={devdogCobranded}
+                />
+              </div>
 
-            <div className="flex items-center text-lg font-bold xl:text-xl">
-              <p className="-mr-6 flex-1 cursor-default rounded-l-full bg-[#F8E6EA] bg-dusty-pink px-4 py-3 text-left text-neutral-600/40 shadow-inner">
-                Ready?
-              </p>
-              <Link
-                className="flex items-center gap-10 rounded-full border-2 border-red-900 bg-bulldog-red py-2.5 pl-4 pr-1.5 text-white shadow-md"
-                href="/create"
-              >
-                Get Started!{" "}
-                <span className="-my-2 flex size-10 items-center justify-center rounded-full border-2 border-red-900 bg-white pt-0.5 text-[1.5rem] leading-none shadow-sm">
-                  🚀
-                </span>
-              </Link>
+              <div className="flex items-center justify-center text-xl font-bold xl:text-2xl w-full max-w-lg">
+                <p className="-mr-8 w-1/2 cursor-default rounded-l-full bg-[#F8E6EA] bg-dusty-pink px-6 py-5 text-left text-neutral-600/40 shadow-inner">
+                  Ready?
+                </p>
+                <Link
+                  className="w-3/5 flex items-center gap-4 rounded-full border-2 border-red-900 bg-bulldog-red py-4 pl-8 pr-1.5 text-white shadow-md"
+                  href="/create"
+                >
+                  Get Started!{" "}
+                  <span className="-my-2 flex size-12 items-center justify-center rounded-full border-2 border-red-900 bg-white pt-0.5 text-[1.5rem] leading-none shadow-sm ml-auto mr-1">
+                    🚀
+                  </span>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,15 +1,25 @@
 import { PiInstagramLogoBold, PiLinkedinLogoBold } from "react-icons/pi";
+import Image from 'next/image';
 
 export function Footer() {
   return (
-    <footer className="m-4 grid grid-cols-6 grid-rows-3 gap-y-1.5 rounded-md border-2 border-neutral-500 bg-neutral-200 px-4 py-3 text-neutral-600 min-[480px]:grid-cols-[1fr_repeat(3,1.5fr)_1fr] min-[480px]:grid-rows-2 min-[480px]:gap-y-2 min-[480px]:py-4 sm:gap-y-1.5 sm:py-3 md:grid-cols-[repeat(2,max-content)_1fr_repeat(3,max-content)] md:grid-rows-1 md:gap-x-4 lg:gap-x-6 lg:px-6">
-      <h2 className="col-span-full text-center text-slate-900 sm:row-start-2 md:col-span-1 md:col-start-3 md:row-start-1">
-        Developed by UGA DevDogs &copy;
-      </h2>
+    <footer className="m-4 grid grid-cols-6 grid-rows-3 gap-y-0 rounded-md border-2 border-neutral-500 bg-neutral-200 px-4 py-0 text-neutral-600 min-[480px]:grid-cols-[1fr_repeat(3,1.5fr)_1fr] min-[480px]:grid-rows-2 min-[480px]:gap-y-1.5 min-[480px]:py-3 sm:gap-y-1 sm:py-2 md:grid-cols-[repeat(2,max-content)_1fr_repeat(3,max-content)] md:grid-rows-1 md:gap-x-4 lg:gap-x-6 lg:px-6">
+      {/* WordmarkMascot image */}
+      <div className="col-span-full flex justify-center items-center h-full py-1 sm:row-start-2 md:col-span-1 md:col-start-3 md:row-start-1">
+        <Image
+          src="/images/WordmarkMascot.png"
+          alt="UGA DevDogs and GDG Logos"
+          width={250}
+          height={40}
+          className="object-contain mx-auto"
+          priority
+        />
+      </div>
 
       <nav className="contents">
         <a
-          className="col-span-2 text-center hover:text-slate-900 min-[480px]:col-span-1 min-[480px]:col-start-2 md:col-start-1 md:row-start-1"
+          rel="noopener"
+          className="col-span-2 flex items-center justify-center text-center hover:text-slate-900 min-[480px]:col-span-1 min-[480px]:col-start-2 md:col-start-1 md:row-start-1"
           href="https://devdogs.uga.edu/"
           target="_blank"
         >
@@ -17,28 +27,32 @@ export function Footer() {
         </a>
 
         <a
-          className="col-span-2 text-center hover:text-slate-900 min-[480px]:col-span-1"
+          className="col-span-2 flex items-center justify-center text-center hover:text-slate-900 min-[480px]:col-span-1"
           href="#"
         >
           Terms of Use
         </a>
 
         <a
-          className="col-span-2 text-center hover:text-slate-900 min-[480px]:col-span-1"
+          className="col-span-2 flex items-center justify-center text-center hover:text-slate-900 min-[480px]:col-span-1"
           href="#"
         >
           Contact
         </a>
 
         <a
-          className="col-span-3 flex justify-end px-2 text-2xl text-neutral-500 hover:text-slate-900 min-[480px]:col-span-1 min-[480px]:col-start-1 min-[480px]:row-start-2 min-[480px]:justify-center sm:row-start-1 md:col-start-5 md:px-0"
+          title="Instagram"
+          rel="noopener"
+          className="col-span-3 flex items-center justify-end px-2 text-2xl text-neutral-500 hover:text-slate-900 min-[480px]:col-span-1 min-[480px]:col-start-1 min-[480px]:row-start-2 min-[480px]:justify-center sm:row-start-1 md:col-start-5 md:px-0"
           href="https://www.instagram.com/devdogs_uga/"
           target="_blank"
         >
           <PiInstagramLogoBold />
         </a>
         <a
-          className="col-span-3 flex justify-start px-2 text-2xl text-neutral-500 hover:text-slate-900 min-[480px]:col-span-1 min-[480px]:justify-center md:px-0"
+          title="LinkedIn"
+          rel="noopener"
+          className="col-span-3 flex items-center justify-start px-2 text-2xl text-neutral-500 hover:text-slate-900 min-[480px]:col-span-1 min-[480px]:justify-center md:px-0"
           href="https://www.linkedin.com/company/devdogs-uga/"
           target="_blank"
         >
