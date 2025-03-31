a4b3d7ac8c0b80aded303def05602fcee1525fc1
hilton326 <hjd59728@uga.edu>
2025-03-31 18:20:44 -0500

    Fixed arrows/schedule titles not centering properly on schedule selection

--- frontend/src/app/schedules/page.tsx
+++ frontend/src/app/schedules/page.tsx
@@ -17,13 +17,8 @@ export default function SchedulePage() {
   // Render the schedule
   return (
     <div className="mx-auto min-h-screen w-[100%]">
-      <div className="p-7 text-center text-blue-600">
-        <button className="rounded-lg bg-bulldog-red px-4 py-2 font-semibold text-white">
-          <Link href={"/route-map"}>Route Map</Link>
-        </button>
-      </div>
       {/* Schedule display container */}
-      <div className="flex flex-grow flex-row overflow-y-auto">
+      <div className="mt-16 flex flex-grow flex-row overflow-y-auto">
         <Suspense fallback={<div>Loading...</div>}>
           <ScheduleDisplay bgColors={bgColors} />
         </Suspense>
